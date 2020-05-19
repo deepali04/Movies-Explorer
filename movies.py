12e69f3a9b3587f0ff50cfa6397fe7af5d3611ef
deepali04 <nagwade.deepali@gmail.com>
2020-05-20 03:20:33 +0530

    Added comments

--- movies.py
+++ movies.py
@@ -1,12 +1,13 @@
-import requests                                                                    #
-from bs4 import BeautifulSoup
+import requests                #For making request to a web page and print response text                                                                 
+from bs4 import BeautifulSoup  #Python library for web scraping (for puling data out of tml and xml file)
 
 
 def movies_in_theatre(url):
-    code=requests.get(url)
-    text=code.text
-    soup=BeautifulSoup(text,"html.parser")
-    movie_name="Movies_near_me @BookmyShow"+"\n\n"
+    
+    code=requests.get(url)                          #Response Object (code) --- sending GET request to specified URL
+    text=code.text                                  #Reading text from response object
+    soup=BeautifulSoup(text,"html.parser")          #Beautiful soup object (soup) --- parsing it to Beautifulsoup constructor
+    movie_name="Movies_near_me @BookmyShow"+"\n\n"  
     i=1
     for link in soup.findAll('a',{'class':'__movie-name'}):
         title=link.get('title')
